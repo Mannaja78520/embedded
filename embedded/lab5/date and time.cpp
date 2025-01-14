7b077bdf7d354e158f0e5e1e997fffd7c9571b58
My Name <myemail@example.com>
2025-01-14 16:01:29 +0700

    14/1

---
+++ embedded/lab5/date and time.cpp
@@ -0,0 +1,114 @@
+#include <Arduino.h>
+#include <Wire.h>  // Include Arduino Wire library for I2C communication
+#include <SPI.h>
+#include <Adafruit_I2CDevice.h>
+#include "RTClib.h"
+#include <LCD_I2C.h>
+
+LCD_I2C lcd(0x27, 16, 2);
+RTC_DS1307 rtc;
+
+char daysOfTheWeek[7][12] = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+
+void setup () {
+
+  Serial.begin(115200);
+  lcd.begin(); // If you are using more I2C devices using the Wire library use lcd.begin(false)
+                 // this stop the library(LCD_I2C) from calling Wire.begin()
+    // Note: On ESP32 you would rather use:
+    //
+    //   lcd.begin(4,5);
+    //
+    // which maps 'Wire' sda to pin 4 and scl to pin 5 in this example.
+
+    lcd.backlight();
+
+#ifndef ESP8266
+  while (!Serial); // wait for serial port to connect. Needed for native USB
+#endif
+
+  if (! rtc.begin()) {
+    Serial.println("Couldn't find RTC");
+    Serial.flush();
+    while (1) delay(10);
+  }
+
+  if (! rtc.isrunning()) {
+    Serial.println("RTC is NOT running, let's set the time!");
+    // When time needs to be set on a new device, or after a power loss, the
+    // following line sets the RTC to the date & time this sketch was compiled
+    rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+    // This line sets the RTC with an explicit date & time, for example to set
+    // January 21, 2014 at 3am you would call:
+    // rtc.adjust(DateTime(2014, 1, 21, 3, 0, 0));
+  }
+
+  // When time needs to be re-set on a previously configured device, the
+  // following line sets the RTC to the date & time this sketch was compiled
+  // rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+  // This line sets the RTC with an explicit date & time, for example to set
+  // January 21, 2014 at 3am you would call:
+  // rtc.adjust(DateTime(2014, 1, 21, 3, 0, 0));
+}
+
+void loop () {
+  lcd.clear();
+    DateTime now = rtc.now();
+
+    Serial.print(now.year(), DEC);
+    Serial.print('/');
+    Serial.print(now.month(), DEC);
+    Serial.print('/');
+    Serial.print(now.day(), DEC);
+    Serial.print(" (");
+    Serial.print(daysOfTheWeek[now.dayOfTheWeek()]);
+    Serial.print(") ");
+    Serial.print(now.hour(), DEC);
+    Serial.print(':');
+    Serial.print(now.minute(), DEC);
+    Serial.print(':');
+    Serial.print(now.second(), DEC);
+    Serial.println();
+
+    Serial.print(" since midnight 1/1/1970 = ");
+    Serial.print(now.unixtime());
+    Serial.print("s = ");
+    Serial.print(now.unixtime() / 86400L);
+    Serial.println("d");
+
+    // calculate a date which is 7 days, 12 hours, 30 minutes, and 6 seconds into the future
+    DateTime future (now + TimeSpan(7,12,30,6));
+
+    Serial.print(" now + 7d + 12h + 30m + 6s: ");
+    Serial.print(future.year(), DEC);
+    Serial.print('/');
+    Serial.print(future.month(), DEC);
+    Serial.print('/');
+    Serial.print(future.day(), DEC);
+    Serial.print(' ');
+    Serial.print(future.hour(), DEC);
+    Serial.print(':');
+    Serial.print(future.minute(), DEC);
+    Serial.print(':');
+    Serial.print(future.second(), DEC);
+    Serial.println();
+
+    Serial.println();
+
+   
+    lcd.print(now.year(), DEC);
+    lcd.print('/');
+    lcd.print(now.month(), DEC);
+    lcd.print('/');
+    lcd.print(now.day(), DEC);
+    lcd.setCursor(0,2);
+    lcd.print(now.hour(), DEC);
+    lcd.print(':');
+    lcd.print(now.minute(), DEC);
+    lcd.print(':');
+    lcd.print(now.second(), DEC);
+    delay(1000);
+ 
+
+
+}
--- embedded/src/main.cpp
+++ embedded/src/main.cpp
@@ -1,28 +1,78 @@
 #include <Arduino.h>
 #include <Wire.h>  // Include Arduino Wire library for I2C communication
-// #include <Adafruit_Sensor.h>  // Include Adafruit Sensor library
-#include <Adafruit_PCF8574.h>  // Include Adafruit PCF8574 library
+#include <SPI.h>
+#include <Adafruit_I2CDevice.h>
+#include "RTClib.h"
+#include <LCD_I2C.h>
 
-#define PCF8574_Address 0x20
-/* Example for 1 output LED that is connected from power to the GPIO expander pin #7
- * Note the LEDs must be connected with the CATHODES to the expander, to SINK current!
- * The PCF8574 cannot SOURCE current!
- */
+LCD_I2C lcd(0x27, 16, 2); // Default address of most PCF8574 modules, change according
 
-#define PCF8574_Address 0x20
-
-Adafruit_PCF8574 pcf;
-void setup() {
-  Serial.begin(9600);
-  Serial.println("Adafruit PCF8574 LED blink test");
-  if (!pcf.begin(PCF8574_Address, &Wire)) {
-    Serial.println("Couldn't find PCF8574");
-  }
-  pcf.pinMode(7, OUTPUT);
+void setup()
+{
+    lcd.begin();
+    lcd.backlight();
 }
-void loop() {
-  pcf.digitalWrite(7, LOW);  // turn LED on by sinking current to ground
-  delay(1000);
-  pcf.digitalWrite(7, HIGH); // turn LED off by turning off sinking transistor
-  delay(1000);
+
+void loop()
+{
+    // Display Hello, World! with the cursor
+    lcd.print("     Hello, World!"); // Display the message
+    lcd.setCursor(5, 1); // Move the cursor to the second row, after the message
+    lcd.blink(); // Start blinking the cursor
+
+    delay(2000); // Keep the cursor blinking for 2 seconds
+
+    lcd.clear(); // Clear the display
+
+    //Autoscroll
+    lcd.print("     Hello, World!"); // You can make spaces using well... spaces
+    lcd.setCursor(5, 1); // Or setting the cursor in the desired position.
+    delay(500);
+    lcd.setCursor(5, 0);
+    lcd.setCursor(10, 1);
+    lcd.autoscroll();
+
+    for (int i = 0; i < 10; i++)
+    {
+        lcd.print(i);
+        delay(200);
+    }
+
+    lcd.noAutoscroll();
+    lcd.clear();
+
+    // Scroll left and right
+    lcd.setCursor(10, 0);
+   
+    for (int i = 0; i < 10; i++)
+    {
+        lcd.scrollDisplayLeft();
+        delay(200);
+    }
+    lcd.clear();
+    for (int i = 0; i < 10; i++)
+    {
+        lcd.scrollDisplayRight();
+        delay(200);
+    }
+    lcd.clear();
+
+    //Cursor
+    lcd.setCursor(0, 0);
+    lcd.cursor();
+    delay(3000);
+    lcd.clear();
+
+    //Cursor blink
+    lcd.setCursor(0, 0);
+    lcd.blink();
+    delay(3000);
+    lcd.clear();
+
+    //Blink without cursor
+    lcd.setCursor(0, 0);
+    lcd.noCursor();
+    delay(3000);
+    lcd.noBlink();
+    lcd.clear();
 }
